171332b02fe40c75fd79ba64471a8779356fc3ad
Dipan Mallick <dipanmallick39@gmail.com>
2026-01-27 23:26:12 +0530

    commit 2

--- movie_ticketing_app/lib/main.dart
+++ movie_ticketing_app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp  extends StatelessWidget{
   Widget build(BuildContext build){
     return Scaffold(
       body: Container(
-        child: Text('today was a waste full day '),
+        child: Text('yeahaaaaaaaaaa '),
       ),
     );
   }
