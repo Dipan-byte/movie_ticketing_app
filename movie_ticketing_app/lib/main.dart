2f18587b3d965677f1f2cf23631a87f056769754
Dipan Mallick <dipanmallick39@gmail.com>
2026-01-28 23:07:28 +0530

    doing this

--- movie_ticketing_app/lib/main.dart
+++ movie_ticketing_app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp  extends StatelessWidget{
   Widget build(BuildContext build){
     return Scaffold(
       body: Container(
-        child: Text('yeahaaaaaaaaaa '),
+        child: Text('starting boyes '),
       ),
     );
   }
