356fceb69af7e80f07c51c14be9fa59d54f16a12
Dipan Mallick <dipanmallick39@gmail.com>
2026-02-02 23:55:19 +0530

    done

--- movie_ticketing_app/lib/main.dart
+++ movie_ticketing_app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp  extends StatelessWidget{
         title: Text('hey nerds'),
       ),
       body: Container(
-        child: Center(child: Text('ohh')),
+        child: Center(child: Text('ohh0')),
       ),
     );
   }
